01136836f365b34d2009cb94e7981360556e9ac8
xuhongg <40853948+xuhongg@users.noreply.github.com>
2022-04-15 19:12:40 +0800

    Add files via upload
    webpack root path change、add amfe-flexible

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
-import less from 'less'
-Vue.use(less)
+import App from './App'
+import 'amfe-flexible'; // 引入lib-flexible
+import router from './router'
+import './main.less'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  components: { App },
+  template: '<App/>'
 })
